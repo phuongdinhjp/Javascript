903ad81bea89c0bf033367f620e1f21b462843c4
Dinh Xuan Phuong <phuongdinhjp@gmail.com>
2024-11-14 17:46:15 +0700

    add new lesson

---
+++ Lesson/main.js
@@ -0,0 +1,33 @@
+// Bai 8 : Bien trong JS
+// var firstName = 'Dinh';
+// var lastName = 'Phuong';
+// var fullName = `${firstName + ' ' + lastName}`;
+// console.log(fullName);
+
+
+// Bai 12 : Thuat ngu Built-in la gi ?
+/**
+ * 
+ * 1 so ham built-in
+ * Alert
+ * Console
+ * Confirm
+ * Prompt
+ * Set timeOut : Chay sau 1 khoang thoi gian va dung lai
+ * Set interval : Chay lien tuc lap di lap lai
+ * 
+ * 
+ */
+
+// confirm('Something to say');
+// prompt('');
+
+// setTimeout(function doSomething() {
+//     alert('Thong bao')
+// }, 1000);
+
+// setInterval(function countNumber() {
+//     for (let i = 0; i <= 5; i++) {
+//         console.log(i);
+//     }
+// }, 5000);

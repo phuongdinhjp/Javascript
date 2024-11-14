560707cadb9bc1a6f027932d3e27ac0481c6b85d
dinh Phuong <phuongdinhjp@gmail.com>
2024-11-14 23:04:20 +0700

    end

---
+++ Lesson/main3.js
@@ -0,0 +1,16 @@
+/**
+ * Bai 14 : Lam quen voi toan tu
+ * 1. Toan tu so hoc - Arithmetric
+ * 2. toan tu gan - Assignment
+ * 3. Toan tu so sanh - Comparison
+ * 4. toan tu logic - Logical
+ */
+
+var a = 1;b = 2;
+console.log(a);
+if (a > b) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}

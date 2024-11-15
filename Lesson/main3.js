feeb6e8a56efda84484f4b8339552eb3959c7cc2
Dinh Xuan Phuong <phuongdinhjp@gmail.com>
2024-11-15 17:38:07 +0700

    news

--- Lesson/main3.js
+++ Lesson/main3.js
@@ -6,11 +6,34 @@
  * 4. toan tu logic - Logical
  */
 
-var a = 1;b = 2;
-console.log(a);
-if (a > b) {
-    console.log(true);
-}
-else {
-    console.log(false);
-}
+
+
+// var a = 1;b = 2;
+// console.log(a);
+// if (a > b) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+/**
+ * Bai 15 : Toan tu so hoc
+ * + --> Cong
+ * - --> Tru
+ * * --> Nhan
+ * ** --> Luy Thua
+ * / --> Chia
+ * % --> Chia lay so du
+ * ++ --> Tang 1 gia tri so
+ * -- --> Giam 1 gia tri so
+ */
+
+
+var a = 8;
+var b = 5;
+var c = a % b;
+console.log(c);
+console.log(++a);
+console.log(a--);
+console.log(++a);
